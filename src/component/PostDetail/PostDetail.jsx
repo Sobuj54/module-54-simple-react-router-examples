c0458c0c5fa104c959321a9477e140ca9eb87285
soubj <sobujahmed124@gmail.com>
2023-04-23 23:21:08 +0600

    going back history with react navigate

--- src/component/PostDetail/PostDetail.jsx
+++ src/component/PostDetail/PostDetail.jsx
@@ -4,14 +4,22 @@ import {
     Link,
     useLoaderData,
   } from "react-router-dom";
+  import { useNavigate } from "react-router-dom";
 
 const PostDetail = () => {
     const post = useLoaderData();
     const {body, title, userId, id} = post;
+
+   const navigate = useNavigate();
+    const goBack = () => {
+        navigate(-1);
+    }
     return (
         <div>
             <h3>this is post details of id : {id}</h3>
+            <h6>{title}</h6>
             <p>{body}</p>
+            <button onClick={goBack}>Go Back</button>
         </div>
     );
 };
