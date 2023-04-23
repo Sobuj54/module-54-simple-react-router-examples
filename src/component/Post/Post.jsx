03073484a690059623175c791516f0a19df5089a
soubj <sobujahmed124@gmail.com>
2023-04-23 23:08:00 +0600

    using useNavigate hook on onClick function

--- src/component/Post/Post.jsx
+++ src/component/Post/Post.jsx
@@ -4,17 +4,24 @@ import {
     Link,
     useLoaderData,
   } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import './Post.css'
 
 const Post = ({post}) => {
     const {id, title} =post;
+
+    const navigate = useNavigate();
+    const clickHandler =() => {
+        navigate(`/post/${id}`);
+    }
+    
     return (
         <div className='post'>
             <h2>id: {id}</h2>
             <h3>Title : {title}</h3>
             <Link to={`/post/${id}`}>show details</Link>
             <Link to={`/post/${id}`}><button>Show post details</button></Link>
-            
+            <button onClick={clickHandler}>with button handler</button>
         </div>
     );
 };
