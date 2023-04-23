35f40a92fd183621923f5d2acb753764d711173a
soubj <sobujahmed124@gmail.com>
2023-04-23 18:12:17 +0600

    basic idea about router and general view of spa

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -4,6 +4,7 @@ const About = () => {
     return (
         <div>
             <h3>this is about component.</h3>
+           
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -9,22 +9,40 @@ import {
 import About from './component/About/About.jsx';
 import Contact from './component/Contact/Contact.jsx';
 import ErrorPage from './component/Error/ErrorPage.jsx';
+import Home from './component/Home/Home.jsx';
+
+// const router = createBrowserRouter([
+//   {
+//     path:'/',
+//     element: <App></App>,
+//     errorElement: <ErrorPage></ErrorPage>
+//   },
+//   {
+//     path: '/about',
+//     element: <About></About>,
+//     errorElement: <ErrorPage></ErrorPage>
+//   },
+//   {
+//     path: '/contact',
+//     element : <Contact></Contact>,
+//     errorElement: <ErrorPage></ErrorPage>
+//   }
+// ])
 
 const router = createBrowserRouter([
   {
     path:'/',
-    element: <App></App>,
-    errorElement: <ErrorPage></ErrorPage>
-  },
-  {
-    path: '/about',
-    element: <About></About>,
-    errorElement: <ErrorPage></ErrorPage>
-  },
-  {
-    path: '/contact',
-    element : <Contact></Contact>,
-    errorElement: <ErrorPage></ErrorPage>
+    element:<Home></Home>,
+    children:[
+      {
+        path: '/about',
+        element: <About></About>
+      },
+      {
+        path : '/contact',
+        element: <Contact></Contact>
+      }
+    ]
   }
 ])
 
